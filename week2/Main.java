3e135079e9692bd5e441367f76ec16fe94476298
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-18 06:28:34 +0000

    Week 2 completed

---
+++ week2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Rain", 21, 3.75);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
