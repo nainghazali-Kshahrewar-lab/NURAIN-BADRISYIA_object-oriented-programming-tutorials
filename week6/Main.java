13f4a02081340d363b4d211cb7f78240929b14d4
nainghazali-Kshahrewar-lab <nainghazali26@gmail.com>
2026-08-18 07:14:24 +0000

    Complete Week 6 employee and lecturer tutorial

---
+++ week6/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lecturer lecturer = new Lecturer(
+            "L100",
+            "Dr Ahmad",
+            "Java Programming",
+            "Faculty of Information Technology"
+        );
+
+        lecturer.displayInfo();
+        lecturer.displaySubject();
+        lecturer.displayDepartment();
+    }
+}
